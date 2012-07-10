eeb30e42a561e02a5c6d82282dd94265f33b8ce8
Nathan Heinrich <nathan.heinrich@gmail.com>
2012-07-09 21:33:47 -0700

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameStatus::Application.config.secret_token = '2df2b01bb7125cefa5e2ca540673afd0a34cdba5fc5bd85790a1ae2d956aeadfa3c7c00c6043138fec083619f5eca248c54f845890838e09c42d00419cb35147'
