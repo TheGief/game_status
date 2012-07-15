4317c931bffbafbc8750e548dc32cfa4e8f44094
Luke Barbuto <luke.barbuto@gmail.com>
2012-07-14 21:36:32 -0700

    prep for test on heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GameStatus::Application.config.secret_token = '2df2b01bb7125cefa5e2ca540673afd0a34cdba5fc5bd85790a1ae2d956aeadfa3c7c00c6043138fec083619f5eca248c54f845890838e09c42d00419cb35147'
+GameStatus::Application.config.secret_token = '7769f2aadf033fd3333701d8927d9a3845c49982a628a07649583305b14fb7830f5f567bcc64ba1eded01976516c4d152571d689bdccf339c14cc8d1965815ab'
