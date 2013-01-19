032b61bc917e05f6813ff1ae158bf215dddbc915
Nate <nathan.heinrich@gmail.com>
2013-01-19 08:03:14 -0800

    login with username, redirect to games

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to games_url, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
