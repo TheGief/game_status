0f0b6b57471cba2936ef88c173bc2ce33475b383
Luke Barbuto <luke.barbuto@gmail.com>
2013-01-19 19:15:44 -0800

    Update sign in spec

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -1,27 +1,23 @@
 require 'spec_helper'
 
 feature "Sign in" do
-
-  background do
-    @user = FactoryGirl.create(:han_solo)
-  end
+  let!(:user) { create(:han_solo) }
 
   scenario "Signing in with correct credentials" do
     visit login_path
-    fill_in 'email', with: @user.email
-    fill_in 'password', with: @user.password
+    fill_in 'username', with: user.username
+    fill_in 'password', with: user.password
     click_button 'Go!'
     page.should have_content('Logout')
-    current_path.should eq(root_path)
+    current_path.should eq(games_path)
   end
 
   scenario "Signing in with incorrect credentials" do
     visit login_path
-    fill_in 'email', with: @user.email
+    fill_in 'username', with: user.username
     fill_in 'password', with: "wrong_password"
     click_button 'Go!'
     page.should have_content('Invalid')
     current_path.should eq(sessions_path)
   end
-
 end
