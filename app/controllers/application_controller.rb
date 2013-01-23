6be425dc784c54159837614ec3d6597a4c307374
Nate <nathan.heinrich@gmail.com>
2013-01-22 18:35:10 -0800

    Redirect to friends on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
   # force_ssl
+
+  def after_sign_in_path_for(resource)
+    friends_path
+  end
 end
