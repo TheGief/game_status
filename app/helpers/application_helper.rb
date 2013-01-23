fcc7e4b0385181ef9f94c000342d63c581cce198
Luke Barbuto <luke.barbuto@gmail.com>
2013-01-23 00:08:35 -0800

    Fix broken cover_img helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def cover_img(item, edit = false)
-    path = edit ? send("edit_#{item}_path(item)") : item
+    path = edit ? eval("edit_#{item.class.to_s.downcase}_path(item)") : item
     link_to image_tag(item.image_url, class: 'cover_img'), path
   end
 end
