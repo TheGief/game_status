fd0dc6c3d43f980d1f82252ce103a9d711d3cb11
Luke Barbuto <luke.barbuto@gmail.com>
2013-01-22 20:16:28 -0800

    Configure ActionMailer for Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 GameStatus::Application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
+}
+ActionMailer::Base.delivery_method ||= :smtp
