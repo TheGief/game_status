b5b8c0118860efcc87c7d01c06bc773287e7e89b
Nate <nathan.heinrich@gmail.com>
2013-01-26 14:37:08 -0800

    Virtual attributes for start/end, validation error partial
    *better validation for start/end
    *wish Chronic could parse '2hrs' not just '2 hrs'
    *might move duration to a select with common values
    *.stack class added to remove bottom margin from elements

--- app/controllers/play_times_controller.rb
+++ app/controllers/play_times_controller.rb
@@ -8,12 +8,11 @@ class PlayTimesController < ApplicationController
   def create
     @play_time = PlayTime.new(params[:play_time])
     @play_time.user_id = current_user.id
-    @play_time.start = Chronic.parse(params[:play_time][:start])
-    @play_time.end = Chronic.parse(params[:play_time][:end] + ' from ' + params[:play_time][:start])
-
+    
     @play_times = current_user.play_times.future
 
     if @play_time.save
+      # TODO: send list of friends a notification
       redirect_to play_path, notice: 'Play time was successfully created.'
     else
       render action: 'new', params: params[:play_time]
--- app/models/play_time.rb
+++ app/models/play_time.rb
@@ -1,18 +1,63 @@
 class PlayTime < ActiveRecord::Base
 
-  attr_accessible :start, :end, :game_id, :console_id, :user_id
+  attr_accessible :start_time, :duration_text, :game_id, :console_id, :user_id
+  attr_writer :start_time
+  attr_writer :duration_text
 
   belongs_to :user
   belongs_to :game
   belongs_to :console
 
-  scope :future, lambda { where("play_times.end > ?", Time.current ) }
+  scope :future, lambda { where("play_times.end > ?", Time.current) }
 
-  before_create :set_duration
-
-  # 
-  validate :dates_parseable?
   validates :game_id, :console_id, :presence => true
+  validate :check_start_time, :check_duration_text
+
+  before_save :save_start_time, :save_duration_text
+  before_create :set_duration, :set_user_id
+
+  # start_time virtual attribute
+  def start_time
+    @start_time || Chronic.parse(self.start)
+  end
+
+  def save_start_time
+    self.start = Chronic.parse(@start_time) if @start_time.present?
+  end
+
+  def check_start_time
+    logger.info @start_time.to_yaml
+    logger.info @duration_text.to_yaml
+    if !@start_time.present?
+      errors.add "Start time", "(at) is required"
+    elsif @start_time.present? && Chronic.parse(@start_time).nil?
+      errors.add "Start time", "invalid, try something like '6pm' or '10:15am'"
+    end
+  rescue ArgumentError
+    errors.add "Start time", "is out of range"
+  end
+
+  # duration_text virtual attribute
+  def duration_text
+    @duration_text || ""
+  end
+
+  def save_duration_text
+    self.end = Chronic.parse(@duration_text + " from " + @start_time) if @duration_text.present?
+  end
+
+  def check_duration_text
+    logger.info @start_time.to_yaml
+    logger.info @duration_text + " from " + @start_time
+    if !@duration_text.present?
+      errors.add "Duration", "(for) is required"
+    elsif @duration_text.present? && Chronic.parse(@duration_text + " from " + @start_time).nil?
+      errors.add "Duration", "invalid, try something like '2 hrs' or '90 mins'"
+    end
+  rescue ArgumentError
+    errors.add "Duration", "is out of range"
+  end
+
 
   def in_progress?
     return true if self.start < Time.current && self.end > Time.current
@@ -20,12 +65,6 @@ class PlayTime < ActiveRecord::Base
   end
 
   private
-
-  def dates_parseable?
-    errors.add("Start Time", "invalid, try something like '6pm' or '10:15am'") unless Chronic.parse(self.start)
-    errors.add("Duration", "invalid, try something like '2 hrs' or '90 mins'") unless Chronic.parse(self.end)
-    logger.info errors.to_yaml
-  end
 
   def set_duration
     self.duration = (self.end - self.start) / 60 
