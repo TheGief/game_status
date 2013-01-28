ed262a870546d6625704d0dc2066781ec445d483
Luke Barbuto <luke.barbuto@gmail.com>
2013-01-27 18:19:03 -0800

    No need to spell out FactoryGirl

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,7 +2,7 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:han_solo)
+      user = create(:han_solo)
       sign_in user
     end
   end
