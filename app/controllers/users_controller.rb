b9b5d273563582c0c5c9ee6d1565c225bd87451e
Nate <nathan.heinrich@gmail.com>
2013-03-03 22:39:38 -0800

    Fix friending UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def index
     @users = User.select("users.id,username,friendships.status")
-      .joins("LEFT JOIN friendships ON users.id = friendships.user_id AND friendships.friend_id = #{current_user.id}")
-      .limit(30)
+      .joins("LEFT JOIN friendships ON users.id = friendships.friend_id AND friendships.user_id = #{current_user.id}")
+      .limit(50)
   end
 
   def show
