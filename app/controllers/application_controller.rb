51ef147cd698ea6dee53eaa1ad42bcb043b08741
Nate <nathan.heinrich@gmail.com>
2013-03-07 20:06:27 -0800

    Refactor request based time zone setting
    https://github.com/rails/rails/blob/master/activesupport/lib/active_supp
    ort/core_ext/time/zones.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate_user!
-  around_filter :user_time_zone, :if => :current_user
+  around_filter :set_time_zone
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || play_path
   end
 
-  def user_time_zone(&block)
-    Time.use_zone(current_user.time_zone, &block)
+  def set_time_zone
+    if user_signed_in?
+      Time.use_zone(current_user.time_zone) { yield }
+    else
+      yield
+    end
   end
 
 end
